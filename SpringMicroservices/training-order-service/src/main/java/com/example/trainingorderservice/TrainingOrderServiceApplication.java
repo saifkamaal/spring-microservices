479250f719d11d59541726faf2ba43f4b521cc50
Saif Kamaal <saifkamaal@gmail.com>
2022-08-21 09:17:09 +0400

    Update TrainingOrderServiceApplication.java

--- SpringMicroservices/training-order-service/src/main/java/com/example/trainingorderservice/TrainingOrderServiceApplication.java
+++ SpringMicroservices/training-order-service/src/main/java/com/example/trainingorderservice/TrainingOrderServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class TrainingOrderServiceApplication {
 
+	//Comment
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingOrderServiceApplication.class, args);
 	}
